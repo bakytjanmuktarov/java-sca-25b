6ec9c9630baca4e316f7c9de4104d1eabbadc6d6
bakytjanmuktarov <bakytjan.q@gmail.com>
2026-09-24 02:08:51 +0600

    third task)

---
+++ third.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class third {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int k = input.nextInt();
+
+        System.out.println(k / n);
+    }
+}
