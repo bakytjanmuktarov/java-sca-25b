6a02faf58cb109bd98c5c103a7019c371f4d97f5
bakytjanmuktarov <bakytjan.q@gmail.com>
2026-09-24 02:13:31 +0600

    eight task)

---
+++ eight.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class eight {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+        System.out.println((number / 10)% 10);
+    }
+}
