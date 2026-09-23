28170bc75e7637530f7eb04bccdebca831212932
bakytjanmuktarov <bakytjan.q@gmail.com>
2026-09-23 22:24:20 +0600

    learning to commit

--- Main.java
+++ Main.java
@@ -6,9 +6,9 @@ public class Main {
         int a = input.nextInt();
         int b = input.nextInt();
         int n = input.nextInt();
-        int z = (a * 100 + b) * n;
-        int rb = z / 100;
-        int k = z % 100;
+        int x = (a * 100 + b) * n;
+        int rb = x / 100;
+        int k = x % 100;
         System.out.println(rb + " " + k);
     }
 }
